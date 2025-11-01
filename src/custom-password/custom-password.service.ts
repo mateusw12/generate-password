43237a1946df917dd7c73bf28b2ce52f6f10340f
mateus.walz <mateus.walz@gmail.com>
2025-11-01 16:01:41 -0300

    add generate hex password

--- src/custom-password/custom-password.service.ts
+++ src/custom-password/custom-password.service.ts
@@ -12,6 +12,9 @@ export class CustomPasswordService {
   private readonly CONSONANTS = 'bcdfghjklmnpqrstvwxyz';
   private readonly VOWELS = 'aeiou';
 
+  private readonly HEX_CHARS_LOWER = '0123456789abcdef';
+  private readonly HEX_CHARS_UPPER = '0123456789ABCDEF';
+
   generate(dto: CustomPasswordDto): string[] {
     switch (dto.type) {
       case PasswordType.CUSTOM:
@@ -20,6 +23,8 @@ export class CustomPasswordService {
         return this.generateSimple(dto);
       case PasswordType.PRONUNCIABLE:
         return this.generatePronunciable(dto);
+      case PasswordType.HEX:
+        return this.generateHex(dto);
       default:
         throw new Error('Tipo de senha inválido');
     }
@@ -77,21 +82,32 @@ export class CustomPasswordService {
           char = this.VOWELS[Math.floor(Math.random() * this.VOWELS.length)];
         }
 
-        // Aleatoriza maiúsculas
         if (useUppercase && Math.random() > 0.5) char = char.toUpperCase();
-
-        // Aleatoriza números em posições aleatórias
         if (useNumbers && Math.random() < 0.2) {
           char = this.NUMBERS[Math.floor(Math.random() * this.NUMBERS.length)];
         }
 
         password += char;
-        useConsonant = !useConsonant; // alterna entre consoante e vogal
+        useConsonant = !useConsonant;
       }
 
       passwords.push(password);
     }
 
+    return passwords;
+  }
+
+  private generateHex(dto: CustomPasswordDto): string[] {
+    const chars = dto.uppercase ? this.HEX_CHARS_UPPER : this.HEX_CHARS_LOWER;
+    const passwords: string[] = [];
+    for (let i = 0; i < (dto.count || 1); i++) {
+      let password = '';
+      for (let j = 0; j < dto.length; j++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+      }
+      passwords.push(password);
+    }
     return passwords;
   }
 }
