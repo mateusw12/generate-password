0aa913a8600844b652476baae96d4925bd8ee6c2
mateus.walz <mateus.walz@gmail.com>
2025-11-01 16:19:29 -0300

    add modern encrypt argon2

--- src/encrypt-password/encrypt-password.controller.ts
+++ src/encrypt-password/encrypt-password.controller.ts
@@ -17,6 +17,22 @@ export class EncryptPasswordController {
     return { hash };
   }
 
+  @Post("modern-encrypt")
+  @ApiOperation({ summary: 'Criptografa uma senha usando bcrypt' })
+  @ApiResponse({ status: 201, description: 'Senha criptografada com sucesso.' })
+  async modernEncrypt(@Body() dto: EncryptPasswordDto): Promise<{ hash: string }> {
+    const hash = await this.service.modernEncrypt(dto);
+    return { hash };
+  }
+
+  @Post('modern-compare')
+  @ApiOperation({ summary: 'Compara uma senha com um hash existente' })
+  @ApiResponse({ status: 200, description: 'Resultado da comparação.' })
+  async modernCompare(@Body() dto: ComparePasswordDto): Promise<{ match: boolean }> {
+    const match = await this.service.modernCompare(dto.password, dto.hash);
+    return { match };
+  }
+
   @Post('compare')
   @ApiOperation({ summary: 'Compara uma senha com um hash existente' })
   @ApiResponse({ status: 200, description: 'Resultado da comparação.' })
--- src/encrypt-password/encrypt-password.service.ts
+++ src/encrypt-password/encrypt-password.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { EncryptPasswordDto } from './dto/encrypt-password.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class EncryptPasswordService {
@@ -8,6 +9,23 @@ export class EncryptPasswordService {
     const saltRounds = dto.saltRounds ?? 10;
     const hash = await bcrypt.hash(dto.password, saltRounds);
     return hash;
+  }
+
+  async modernEncrypt(dto: EncryptPasswordDto): Promise<string> {
+    const timeCost = dto.saltRounds ?? 3; // padrão 3 iterações
+    const memoryCost = 2 ** 16; // 64MB de memória
+    const parallelism = 1; // paralelismo simples
+
+    return await argon2.hash(dto.password, {
+      type: argon2.argon2id,
+      timeCost,
+      memoryCost,
+      parallelism,
+    });
+  }
+
+  async modernCompare(password: string, hash: string): Promise<boolean> {
+    return await argon2.verify(hash, password);
   }
 
   async compare(password: string, hash: string): Promise<boolean> {
